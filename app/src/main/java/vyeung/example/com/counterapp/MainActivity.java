91a35f4104f0578dbb4066ef2c844062793cb537
yeungv07 <39495815+yeungv07@users.noreply.github.com>
2018-07-08 13:59:02 -0600

    Added light and dark switch which changes the color scheme of the app. Still need to make the switch fully visible in dark scheme.

--- app/src/main/java/vyeung/example/com/counterapp/MainActivity.java
+++ app/src/main/java/vyeung/example/com/counterapp/MainActivity.java
@@ -1,14 +1,28 @@
 package vyeung.example.com.counterapp;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String WHITE = new String("#ffffff");
+    String BLACK = new String("#000000");
+    String GREY = new String("#d8d5d0");
+    String RED = new String("#ff0000");
+
     protected int count = 0;
     TextView displayCount;
+    Switch lightSwitch;
+    ConstraintLayout main_layout;
+    Button incButton, decButton, resetButton;
+
 
 
     @Override
@@ -17,6 +31,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         displayCount = findViewById(R.id.countValue);
+        lightSwitch = findViewById(R.id.lightSwitch);
+        main_layout = (ConstraintLayout) findViewById(R.id.main_layout);
+        incButton = findViewById(R.id.incButton);
+        decButton = findViewById(R.id.decButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        lightSwitch.setChecked(Boolean.TRUE);
+        incButton.setBackgroundColor(Color.parseColor(GREY));
+        decButton.setBackgroundColor(Color.parseColor(GREY));
+        resetButton.setBackgroundColor(Color.parseColor(RED));
+
+        lightSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    setLightScheme();
+
+                }
+                else {
+                    setDarkScheme();
+                }
+            }
+        });
 
     }
 
@@ -36,4 +73,20 @@ public class MainActivity extends AppCompatActivity {
         count = 0;
         displayCount.setText(Integer.toString(count));
     }
+
+    protected void setLightScheme() {
+        main_layout.setBackgroundColor(Color.parseColor(WHITE));
+        displayCount.setTextColor(Color.parseColor(BLACK));
+        lightSwitch.setText("Light");
+        lightSwitch.setTextColor(Color.parseColor(BLACK));
+    }
+
+    protected void setDarkScheme() {
+        main_layout.setBackgroundColor(Color.parseColor(BLACK));
+        displayCount.setTextColor(Color.parseColor(WHITE));
+        lightSwitch.setText("Dark");
+        lightSwitch.setTextColor(Color.parseColor(WHITE));
+
+    }
+
 }
